29bb5e83ba8a630baffe5bbb33214a8b8949dce1
Dan Flettre <fletd01@yahoo.com>
2014-03-23 14:47:19 -0500

    initial commit

---
+++ desktop/src/net/brokenspork/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package net.brokenspork.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import net.brokenspork.core.SpaceshipWarrior;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new SpaceshipWarrior(), config);
+	}
+}
